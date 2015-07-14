8165a4c053e015a6c66d996adca51fbd6c9ec30f
Viktor Mitev <vmmitev@gmail.com>
2015-07-14 19:23:14 +0300

    added nuget packaging build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 "use strict";
 
+// Configuration
 var config = {
     sourcesDir: "src",
     testsDir: "test",
+    buildDir: "build",
     outputDir: "dist",
     allTS: "/**/*.ts",
     allDTS: "/**/*.d.ts",
     allJS: "/**/*.js",
+    allFiles: "/*.*",
     all: "/*"
 };
 
@@ -18,18 +21,23 @@ var paths = {
     config: config.sourcesDir + "/configuration",
     tsConfig: "/tsconfig.json",
     references: config.sourcesDir + "/**/references.ts",
-    npmExports: config.sourcesDir + "/exports.js"
+    npmExports: config.sourcesDir + "/exports.js",
+    nugetExe: config.buildDir + "/nuget.exe"
 };
 
+// Modules
 var gulp = require("gulp");
+var fs = require('fs');
 var del = require("del");
 var tsc = require("gulp-typescript");
 var empty = require("gulp-empty");
+var nuget = require('gulp-nuget');
 var tslint = require("gulp-tslint");
 var uglify = require("gulp-uglify");
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var addsrc = require('gulp-add-src');
+var request = require('request');
 var replace = require('gulp-replace');
 var jasmine = require("gulp-jasmine");
 var typescript = require('typescript');
@@ -37,6 +45,7 @@ var typescript = require('typescript');
 var tslintReporter = require("./build/reporters/tslint-msbuild.js")
 var testReporter = require("./build/reporters/jasmine-nunit.js");
 
+// Helpers
 var tsProjects = {
     npm: tsc.createProject(config.sourcesDir + paths.tsConfig, { typescript: typescript }),
     core: tsc.createProject(paths.core + paths.tsConfig, { typescript: typescript }),
@@ -64,12 +73,17 @@ function test(minify) {
     return gulp.src(paths.tests)
         .pipe(tsc(tsProjects.tests)).js
         .pipe(minify ? uglify({ mangle: false, output: { beautify: true } }) : empty())
-        .pipe(gulp.dest(config.outputDir))
-        .pipe(jasmine({ reporter: new testReporter.NUnitXmlReporter({ savePath: config.outputDir }) }));
+        .pipe(gulp.dest(config.buildDir))
+        .pipe(jasmine({ reporter: new testReporter.NUnitXmlReporter({ savePath: config.buildDir }) }));
 }
 
+// Build Tasks
 gulp.task("clean", function () {
-    del.sync([config.outputDir + config.all]);
+    del.sync(config.outputDir + config.all);
+});
+
+gulp.task("clean-all", ["clean"], function () {
+    del.sync(config.buildDir + config.allFiles);
 });
 
 gulp.task("lint", function () {
@@ -119,3 +133,21 @@ gulp.task("watch", function () {
 });
 
 gulp.task("default", ["clean", "watch", "test"]);
+
+// Release tasks
+gulp.task("nuget-download", function (done) {
+
+    if (fs.existsSync(paths.nugetExe)) {
+        return done();
+    }
+
+    return request.get("http://nuget.org/nuget.exe")
+        .pipe(fs.createWriteStream(paths.nugetExe));
+});
+
+gulp.task("pack", ["nuget-download", "minify"], function () {
+    gulp.src("")
+        .pipe(nuget.pack({ nuspec: ".nuspec", nuget: paths.nugetExe, version: "0.1.2" }))
+        .pipe(gulp.dest(config.buildDir));
+    //.pipe(nuget.push({ feed: 'http://your-nuget-feed.org/', nuget: nugetPath, apiKey: 'secret-key-goes-here' }));
+});
