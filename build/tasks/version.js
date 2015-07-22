a7f7b8311ffd66979468c45b676ae00c4481ab1f
Viktor Mitev <vmmitev@gmail.com>
2015-07-22 15:49:52 +0300

    added failure message if local changes are present during version bump task

--- build/tasks/version.js
+++ build/tasks/version.js
@@ -1,7 +1,13 @@
-var mversion = require('mversion');
+var mversion = require("mversion"),
+    exec = require("../utils/exec.js");
 
 module.exports = function (version) {
     return function (done) {
+
+        if (exec("git status --porcelain")) {
+            throw new Error("Local changes detected, aborting version bump");
+        }
+
         mversion.update({ version: version, commitMessage: "Bumped to version %s" });
         done();
     };
---
+++ build/utils/exec.js
@@ -0,0 +1,21 @@
+var shell = require("shelljs");
+
+module.exports = function (commandOrOptions, errorMessage) {
+
+    var command = commandOrOptions.command || commandOrOptions,
+        options = {};
+
+
+    if (commandOrOptions.silent) {
+        options.silent = true;
+    }
+
+    errorMessage = errorMessage || "Error executing command: " + command;
+
+    var result = shell.exec(command, options);
+    if (result.code !== 0) {
+        throw new Error(errorMessage);
+    }
+
+    return result.output;
+};
