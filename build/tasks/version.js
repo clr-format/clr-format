e0fa475fbbe6834e5dcb327ee285ede36b23f560
Viktor Mitev <vmmitev@gmail.com>
2015-07-28 22:56:00 +0300

    added commit step to mversion bumps (no tagging) and reverted clean repo check

--- build/tasks/version.js
+++ build/tasks/version.js
@@ -1,8 +1,24 @@
+var files = require("../config/files.js");
+var exec = require("../utils/exec.js");
+
+var format = require("clr-format");
 var mversion = require("mversion");
 
 module.exports = function (version) {
     return function (done) {
-        mversion.update({ version: version });
-        done();
+
+        if (exec("git status --porcelain")) {
+            throw new Error("Local changes detected, aborting version bump");
+        }
+
+        mversion.update({ version: version }, function (err, data) {
+
+            if (err) {
+                throw err;
+            }
+
+            exec(format("git commit -a -m \"preparing version {0}\"", data.newVersion));
+            done();
+        });
     };
 };
