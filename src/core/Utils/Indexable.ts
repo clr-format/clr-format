725244f018fce864c2927cc6f8cd48394e671bbb
Viktor Mitev <vmmitev@gmail.com>
2015-08-27 11:02:48 +0300

    reworded jsdoc

--- src/core/Utils/Indexable.ts
+++ src/core/Utils/Indexable.ts
@@ -1,5 +1,5 @@
 /**
- * Compiler friendly interface that allows objects to have a typed string index accessor and not have to resort to the 'suppressImplicitAnyIndexErrors' compiler option.
+ * Compiler friendly interface that allows objects to have a typed string indexer and not have to resort to the `suppressImplicitAnyIndexErrors` compiler option.
  * @param T The type of object returned by the indexer.
  */
 interface Indexable<T> {
