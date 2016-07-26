73f6e6e5548bde0834fbde403df00f50f7ec15cc
Viktor Mitev <vmmitev@gmail.com>
2016-07-26 21:35:02 +0300

    fixed travis build error

--- build/reporters/tslint-msbuild.js
+++ build/reporters/tslint-msbuild.js
@@ -1,7 +1,7 @@
 module.exports.MSBuild = function (failures, file) {
     failures.forEach(function (failure) {
-        let startLine = failure.startPosition.line + 1;
-        let startPos = failure.startPosition.character + 1;
+        var startLine = failure.startPosition.line + 1;
+        var startPos = failure.startPosition.character + 1;
         console.log(
             `${file.path}(${startLine},${startPos}): error (${failure.ruleName}): ${failure.failure}`);
     });
